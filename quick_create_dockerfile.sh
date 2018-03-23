1cef0c3eec9c5b70599faa27d6f4a5fa55c88aad
Justin <iceyang1007@gmail.com>
2018-03-23 20:19:58 +0800

    some other image

---
+++ quick_create_dockerfile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+name=$1
+
+image=`echo $name | sed 's/:.*//g'`
+tag=`echo $name | sed 's/.*://g'`
+
+dir=$image/$tag
+file=$image/$tag/Dockerfile
+
+echo "mkdir $dir"
+mkdir -p $dir
+
+echo "create Dockerfile"
+if [ -f $file ]; then
+  echo "Error: $file is exists"
+  exit 1
+else
+  echo "FORM $name" > $file
+fi
